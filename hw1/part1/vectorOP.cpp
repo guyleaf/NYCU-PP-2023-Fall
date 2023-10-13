be2eaf099ff6f451a45bac6dcedc09e3cb7077cb
Leaf Ying <leaf.ying.work@gmail.com>
2023-10-13 14:11:20 +0800

    Release second version of clampedExpVector (optimized)

--- hw1/part1/vectorOP.cpp
+++ hw1/part1/vectorOP.cpp
@@ -51,7 +51,8 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
     __pp_vec_int zero = _pp_vset_int(0);
     __pp_vec_int one = _pp_vset_int(1);
     __pp_vec_float maxValue = _pp_vset_float(9.999999f);
-    __pp_mask inputMask, maskIsZero, maskIsNotZero, maskIsGreaterThanZero, maskIsNotClamped;
+    __pp_mask inputMask, maskIsGreaterThanZero, maskIsNotClamped;
+    __pp_mask allMask = _pp_init_ones();
 
     //
     // PP STUDENTS TODO: Implement your vectorized version of
@@ -62,47 +63,18 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
     //
     for (int i = 0; i < N; i += VECTOR_WIDTH)
     {
-        // mask invalid part of vector
-        inputMask = _pp_init_ones(N - i);
-
-        // All zeros
-        maskIsZero = _pp_init_ones(0);
-        maskIsGreaterThanZero = _pp_init_ones(0);
-        maskIsNotClamped = _pp_init_ones(0);
-
         // Load vector of values from contiguous memory addresses
-        // float x = values[i];
-        _pp_vload_float(x, values + i, inputMask);
-        // int y = exponents[i];
-        _pp_vload_int(y, exponents + i, inputMask);
+        _pp_vload_float(x, values + i, allMask);
+        _pp_vload_int(y, exponents + i, allMask);
 
-        // Set result vector to 1.0 as default
-        // result = _pp_vset_float(1.f);
-
-        // if (y == 0) {
-        _pp_veq_int(maskIsZero, y, zero, inputMask);
-
-        // output[i] = 1.f;
-        _pp_vset_float(result, 1.f, maskIsZero);
-
-        // Inverse maskIsZero to generate "else" mask
-        // } else {
-        maskIsNotZero = _pp_mask_not(maskIsZero);
-
-        // Prevent accessing invalid memory when VECTOR_WIDTH not divides N
-        maskIsNotZero = _pp_mask_and(maskIsNotZero, inputMask);
-
-        // float result = x;
-        _pp_vmove_float(result, x, maskIsNotZero);
-
-        // int count = y - 1;
-        _pp_vsub_int(y, y, one, maskIsNotZero);
+        // Set results to 1.0f as default
+        result = _pp_vset_float(1.f);
 
         // while
         while (true)
         {
             // (count > 0)
-            _pp_vgt_int(maskIsGreaterThanZero, y, zero, maskIsNotZero);
+            _pp_vgt_int(maskIsGreaterThanZero, y, zero, allMask);
             if (_pp_cntbits(maskIsGreaterThanZero) == 0)
             {
                 break;
@@ -112,16 +84,15 @@ void clampedExpVector(float *values, int *exponents, float *output, int N)
             _pp_vmult_float(result, result, x, maskIsGreaterThanZero);
 
             // count--;
-            _pp_vsub_int(y, y, one, maskIsGreaterThanZero);
+            _pp_vsub_int(y, y, one, allMask);
         }
 
-        // if (result > 9.999999f) {
-        _pp_vgt_float(maskIsNotClamped, result, maxValue, maskIsNotZero);
+        // Clampe results if > 9.999999f
+        _pp_vgt_float(maskIsNotClamped, result, maxValue, allMask);
+        _pp_vmove_float(result, maxValue, maskIsNotClamped);
 
-        // result = 9.999999f;
-        _pp_vset_float(result, 9.999999f, maskIsNotClamped);
-
-        // } }
+        // mask invalid part of vector
+        inputMask = _pp_init_ones(N - i);
 
         // Write results back to memory
         _pp_vstore_float(output + i, result, inputMask);

666d3b81ba3afd2da2aceabfa8eff880714d17a4
Leaf Ying <leaf.ying.work@gmail.com>
2023-12-30 11:57:37 +0000

    Finish hw6

--- hw6/code/hostFE.c
+++ hw6/code/hostFE.c
@@ -18,63 +18,64 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 
     // create command queue
     cl_command_queue command_queue =
-        clCreateCommandQueue(context, *device, 0, &status);
+        clCreateCommandQueue(*context, *device, 0, &status);
     CHECK(status, "clCreateCommandQueue");
 
     // create input buffers on device memory
-    // cl_mem imageInput =
-    //     clCreateBuffer(context, CL_MEM_READ_ONLY, imageSize, NULL, &status);
-    // CHECK(status, "clCreateBuffer(imageInput)");
-    // cl_mem filterInput =
-    //     clCreateBuffer(context, CL_MEM_READ_ONLY, filterSize, NULL, &status);
-    // CHECK(status, "clCreateBuffer(filterInput)");
+    cl_mem imageInput =
+        clCreateBuffer(*context, CL_MEM_READ_ONLY, imageSize, NULL, &status);
+    CHECK(status, "clCreateBuffer(imageInput)");
+    cl_mem filterInput =
+        clCreateBuffer(*context, CL_MEM_READ_ONLY, filterSize, NULL, &status);
+    CHECK(status, "clCreateBuffer(filterInput)");
 
     // copy image and filter data to buffers
-    // status = clEnqueueWriteBuffer(command_queue, imageInput, CL_NON_BLOCKING,
-    // 0,
-    //                               imageSize, inputImage, 0, NULL, NULL);
-    // CHECK(status, "clEnqueueWriteBuffer(imageInput)");
-    // status = clEnqueueWriteBuffer(command_queue, filterInput,
-    // CL_NON_BLOCKING,
-    //                               0, filterSize, filter, 0, NULL, NULL);
-    // CHECK(status, "clEnqueueWriteBuffer(filterInput)");
+    status = clEnqueueWriteBuffer(command_queue, imageInput, CL_NON_BLOCKING, 0,
+                                  imageSize, inputImage, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer(imageInput)");
+    status = clEnqueueWriteBuffer(command_queue, filterInput, CL_NON_BLOCKING,
+                                  0, filterSize, filter, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer(filterInput)");
 
-    // // create output buffer on device memory
-    // cl_mem imageOutput =
-    //     clCreateBuffer(context, CL_MEM_WRITE_ONLY, imageSize, NULL, &status);
-    // CHECK(status, "clCreateBuffer(imageOutput)");
+    // create output buffer on device memory
+    cl_mem imageOutput =
+        clCreateBuffer(*context, CL_MEM_WRITE_ONLY, imageSize, NULL, &status);
+    CHECK(status, "clCreateBuffer(imageOutput)");
 
-    // // setup conv kernel
-    // cl_kernel convKernel = clCreateKernel(*program, KERNEL_NAME, &status);
-    // clSetKernelArg(convKernel, 0, sizeof(imageInput), (void *)&imageInput);
-    // clSetKernelArg(convKernel, 1, sizeof(filterInput), (void *)&filterInput);
-    // clSetKernelArg(convKernel, 2, sizeof(imageOutput), (void *)&imageOutput);
-    // clSetKernelArg(convKernel, 3, sizeof(imageWidth), (void *)&imageWidth);
-    // clSetKernelArg(convKernel, 4, sizeof(imageHeight), (void *)&imageHeight);
-    // clSetKernelArg(convKernel, 5, sizeof(filterWidth), (void *)&filterWidth);
+    // setup conv kernel
+    cl_kernel convKernel = clCreateKernel(*program, KERNEL_NAME, &status);
+    clSetKernelArg(convKernel, 0, sizeof(imageInput), (void *)&imageInput);
+    clSetKernelArg(convKernel, 1, sizeof(filterInput), (void *)&filterInput);
+    clSetKernelArg(convKernel, 2, sizeof(imageOutput), (void *)&imageOutput);
+    clSetKernelArg(convKernel, 3, sizeof(imageWidth), (void *)&imageWidth);
+    clSetKernelArg(convKernel, 4, sizeof(imageHeight), (void *)&imageHeight);
+    clSetKernelArg(convKernel, 5, sizeof(filterWidth), (void *)&filterWidth);
 
-    // // execute kernel function
-    // size_t globalWorkSize[] = {imageWidth, imageHeight};
+    // execute kernel function
+    size_t globalWorkSize[] = {imageWidth, imageHeight};
     // size_t localWorkSize[] = {WORK_GROUP_SIZE, WORK_GROUP_SIZE};
-    // status =
-    //     clEnqueueNDRangeKernel(command_queue, convKernel, 2, NULL,
-    //                            globalWorkSize, localWorkSize, 0, NULL, NULL);
-    // CHECK(status, "clEnqueueNDRangeKernel");
+    status = clEnqueueNDRangeKernel(command_queue, convKernel, 2, NULL,
+                                    globalWorkSize, NULL, 0, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
 
-    // // wait for kernel
-    // // copy image from device to host memory
-    // status = clEnqueueReadBuffer(command_queue, imageOutput, CL_BLOCKING, 0,
-    //                              imageSize, (void *)outputImage, 0, NULL,
-    //                              NULL);
-    // CHECK(status, "clEnqueueReadBuffer");
+    // wait for kernel
+    // copy image from device to host memory
+    status = clEnqueueReadBuffer(command_queue, imageOutput, CL_BLOCKING, 0,
+                                 imageSize, (void *)outputImage, 0, NULL, NULL);
+    CHECK(status, "clEnqueueReadBuffer(imageOutput)");
 
-    status = clFlush(command_queue);
     status = clFinish(command_queue);
+    CHECK(status, "clFinish");
 
     // clean up all objects created in this function
-    // clReleaseKernel(convKernel);
-    // clReleaseMemObject(imageOutput);
-    // clReleaseMemObject(filterInput);
-    // clReleaseMemObject(imageInput);
-    clReleaseCommandQueue(command_queue);
+    status = clReleaseKernel(convKernel);
+    CHECK(status, "clReleaseKernel");
+    status = clReleaseMemObject(imageOutput);
+    CHECK(status, "clReleaseMemObject(imageOutput)");
+    status = clReleaseMemObject(filterInput);
+    CHECK(status, "clReleaseMemObject(filterInput)");
+    status = clReleaseMemObject(imageInput);
+    CHECK(status, "clReleaseMemObject(imageInput)");
+    status = clReleaseCommandQueue(command_queue);
+    CHECK(status, "clReleaseCommandQueue");
 }
--- hw6/code/kernel.cl
+++ hw6/code/kernel.cl
@@ -3,9 +3,25 @@ __kernel void convolution(__global const float *imageInput,
                           __global float *imageOutput, int imageWidth,
                           int imageHeight, int filterWidth)
 {
-    size_t x = get_global_id(0);
-    size_t y = get_global_id(1);
+    size_t j = get_global_id(0);
+    size_t i = get_global_id(1);
+    int halfFilterSize = filterWidth / 2;
 
     float sum = 0;
-    imageOutput[y * imageWidth + x] = sum;
+    // Apply the filter to the neighborhood
+    for (int k = -halfFilterSize; k <= halfFilterSize; k++)
+    {
+        for (int l = -halfFilterSize; l <= halfFilterSize; l++)
+        {
+            if (i + k >= 0 && i + k < imageHeight && j + l >= 0 &&
+                j + l < imageWidth)
+            {
+                sum += imageInput[(i + k) * imageWidth + j + l] *
+                       filterInput[(k + halfFilterSize) * filterWidth + l +
+                                   halfFilterSize];
+            }
+        }
+    }
+
+    imageOutput[i * imageWidth + j] = sum;
 }
